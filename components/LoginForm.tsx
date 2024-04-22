a4c27d2b61a21baceb29718f91a9621bb554b6e0
Gab0CS <gabocanseco@gmail.com>
2024-04-22 00:58:48 -0600

    Envioroment setup

---
+++ components/LoginForm.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useState } from "react";
+import { supabase } from "@/utils/supabase/client";
+
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <div>LoginForm</div>
+  )
+}
+
+export default LoginForm
