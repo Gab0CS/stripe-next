97c4896a95f1c1a19311113d1cd84d970220d2f4
GabrielCanseco <gabriel.canseco@mtsport.com.mx>
2024-04-23 13:57:56 -0600

    User profile finished, react toast added

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,13 +1,41 @@
 "use client"
-
-import { useState } from "react";
+import { use, useState } from "react";
 import { supabase } from "@/utils/supabase/client";
 
 import React from 'react'
 
 const LoginForm = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleSignUp = async () => {
+    setLoading(true);
+    const randomEmail = `${Math.random().toString(36).substring(7)}@example.com`;
+    const password = 'Password69420'
+
+    const {data, error} = await supabase.auth.signUp({
+      email: randomEmail,
+      password,
+    })
+
+    if(error){
+      console.log(error)
+    } else {
+      console.log("user created and logged in:", data);
+    }
+
+    setLoading(false)
+  };
+
+
   return (
-    <div>LoginForm</div>
+    <button
+    onClick={handleSignUp}
+    disabled={loading}
+    className="mt-5 border rounded-full py-5 px-10 
+    hover:bg-white hover:text-black"
+    >
+      {loading ? "Sign up" : "Sign up with random email and password"}
+    </button>
   )
 }
 
